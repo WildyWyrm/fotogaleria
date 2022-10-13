d8a723d1be62bd40bab8165a6a3938572f73b68e
WildyWyrm <brian_graviti@hotmail.com>
2022-10-13 04:39:07 -0300

    first commit

---
+++ js/galeria.js
@@ -0,0 +1,17 @@
+let galeria = document.getElementById('galeria')
+let fotos = [
+    'anto', 'bruno', 'dani', 'gery',
+    'killq', 'lu', 'maiten', 'maqui',
+    'marce', 'marco', 'marco2', 'solsteiz'
+]
+let cantidad = fotos.length;
+
+//variable auxiliar
+let contenido = '';
+let n = 0;
+while (n < cantidad) {
+    contenido = contenido + '<img src="img/' + fotos[n] + '.png">';
+    n++;
+}
+
+galeria.innerHTML = contenido;
